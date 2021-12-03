ea4f14f56a301803bc9e0c039b2240dea6ab2128
antoniom28 <antomarc@gmail.com>
2021-12-03 15:10:54 +0100

    aggiunta casella pagamento

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ output finale con 2 decimali
 numero da modificare.toFixed(numero di decimali);
 */
 
+/* ESERCITAZIONE ESERCITAZIONE ESERCITAZIONE ESERCITAZIONE ESERCITAZIONE */
+document.writeln(' <h1> ESERCITAZIONE ASSEGNATA </h1> ');
+
 let utenteKm = prompt(' quanti Km devi/vuoi fare? ');
 while(isNaN(utenteKm)){
     utenteKm = (prompt('Errore!! , quanti Km devi/vuoi fare? '));
@@ -31,4 +34,41 @@ if(utenteEta < 18){
     document.writeln('<br> Applicato sconto 40% per over');
 }
 
- document.writeln(' <br>Prezzo biglietto : ' , prezzoTot.toFixed(2) , ' &euro;');
+document.writeln(' <br>Prezzo biglietto : ' , prezzoTot.toFixed(2) , ' &euro;');
+
+ /* BONUS MIO BONUS MIO BONUS MIO BONUS MIO BONUS MIO BONUS MIO */
+let totPrezzo = 0;
+let totSconto = 0;
+let totOrdine = 0;
+let inputKm = 0;
+let inputEta = 0;
+
+function calcolaPrezzo() {
+    inputKm = document.getElementById('inputKm').value;
+    inputEta = document.getElementById('inputEta').value;
+
+    if(inputKm > 0 && inputEta > 0){
+        totPrezzo = 0.21 * inputKm;
+
+    document.getElementById('euroPrezzo').innerHTML =totPrezzo.toFixed(2) + ' &euro;';
+
+    if(inputEta < 18)
+        totSconto = ((totPrezzo * 20) / 100);
+    else if(inputEta > 65)
+        totSconto = ((totPrezzo * 40) / 100);
+        else
+            totSconto = 0;
+    
+    document.getElementById('euroSconto').innerHTML ='- ' + totSconto.toFixed(2) + ' &euro;';
+
+    totOrdine = totPrezzo - totSconto;
+
+    document.getElementById('euroOrdine').innerHTML =totOrdine.toFixed(2) + ' &euro;';
+
+    
+    document.getElementById('inputKm').value = '';
+    document.getElementById('inputEta').value = '';
+    }
+}
+
+
